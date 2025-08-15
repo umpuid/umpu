c9b961eb0297fbc1f52656dd7335bf325ee53d6b
umpuid <umpumeratus@gmail.com>
2025-08-15 21:10:42 +0800

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "UMPU";
+export const SITE_DESCRIPTION = "#umpuitak Budaya Dayak adalah harta tak ternilai. Kitalah pewarisnya, kitalah penjaganya. Jangan biarkan pudar, bagikan pesona Borneo pada dunia.";
