68a5fb850545adcc03812f313bb01907d4e4b303
umpuid <umpumeratus@gmail.com>
2025-08-17 07:19:59 +0800

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "UMPU";
-export const SITE_DESCRIPTION = "#umpuitak Budaya Dayak adalah harta tak ternilai. Kitalah pewarisnya, kitalah penjaganya. Jangan biarkan pudar, bagikan pesona Borneo pada dunia.";
+export const SITE_DESCRIPTION = "Budaya Dayak adalah harta tak ternilai. Kitalah pewarisnya, kitalah penjaganya. Jangan biarkan pudar, bagikan pesona Borneo pada dunia.";
